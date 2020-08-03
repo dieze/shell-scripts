d754a0b99c840504f43f4be1c8761ece7f822e58
Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
2020-08-03 13:24:40 +0200

    Review and fix find-tests.bash

--- find-tests.bash
+++ find-tests.bash
@@ -1,8 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+#
+# $1: file name to print
+#
+print_single() {
+  [ "${1: -4}" = "Test" ] &&
+    printf '%s,%s\n' "-" "$1" ||
+    printf '%s,%s\n' "$1" "-"
+}
 
 column -t -s ',' <(
-  last_file=;
-  found=false;
+  last_file=
+  counter=0
+  found=false
 
   for file in $(
     git log -1 --name-status --pretty= | \
@@ -12,29 +22,40 @@ column -t -s ',' <(
       sed 's/\.java$//' | \
       sort
   ); do
+    ((counter++))
+
     # first loop
-    if [ -z "$last_file" ]; then
-      last_file="$file";
-      continue;
-    fi;
+    [ -z "$last_file" ] && {
+      last_file="$file"
+      continue
+    }
 
     # previous loop found Class/ClassTest pair
     # we must ignore the current last_file
     if $found; then
-      found=false;
-      last_file=;
-      continue;
-    fi;
+      found=false
+      last_file=
+      continue
+    fi
 
-    regexp="^${last_file}Test$";
+    regexp="^${last_file}Test$"
 
     if [[ $file =~ $regexp ]]; then
-      printf "$last_file,$file\n";
-      found=true;
+      printf '%s,%s\n' "$last_file" "$file"
+      found=true
     else
-      printf "$last_file\n";
-    fi;
+      print_single "$last_file"
+    fi
 
-    last_file="$file";
-  done;
-);
+    last_file="$file"
+  done
+
+  # handles single file ; there was only 1 iteration
+  ((counter == 0)) && { print_single "$last_file"; exit 0; }
+
+  # handles last file ; there was n iterations (n being odd number)
+  ((counter % 2 == 1)) && { print_single "$file"; exit 0; }
+
+  # handles last file when found=false ; there was n iterations (n being even number)
+  if ! $found; then print_single "$file"; fi
+)
